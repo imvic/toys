526f846bb99c6261bc7309257d4609c102449dc8
Jia He <jia.he@autodesk.com>
2018-07-16 17:55:01 +0800

    fix(style): refine styles

--- index.jsx
+++ index.jsx
@@ -4,6 +4,28 @@ import * as ReactDOM from "react-dom";
 const ModeQuestion = 0;
 const ModeAnswer = 1;
 
+const styleContainer = {
+  color: "#666",
+  fontFamily: "arial, sans-serif",
+  fontSize: "2rem",
+  margin: "auto",
+  textAlign: "center"
+};
+
+const styleInput = {
+  border: "solid 1px #ccc",
+  padding: "1rem",
+  margin: "1rem 0"
+};
+
+const styleButton = {
+  backgroundColor: "#2ecc71",
+  border: "none",
+  color: "#fff",
+  margin: "1rem 0",
+  padding: "1rem"
+};
+
 class MMathPanel extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -56,26 +78,51 @@ class MMathPanel extends React.PureComponent {
   }
 
   render() {
-    console.log(this.state.op1, this.state.op2);
     const button =
       this.state.mode === ModeQuestion ? (
-        <button onClick={this.onSubmit}>Submit</button>
+        <button style={styleButton} onClick={this.onSubmit}>
+          Submit
+        </button>
       ) : (
-        <button onClick={this.onRefresh}>Refresh</button>
+        <button style={styleButton} onClick={this.onRefresh}>
+          Refresh
+        </button>
       );
     const answer =
-      this.state.mode === ModeAnswer
-        ? `Yours: ${this.state.value} Answer: ${this.state.op1 +
-            this.state.op2}`
-        : "";
+      this.state.mode === ModeAnswer ? (
+        <div>
+          <div>Yours: {this.state.value}</div>
+          <div>Answer: {this.state.op1 + this.state.op2}</div>
+        </div>
+      ) : (
+        ""
+      );
     return (
-      <div>
+      <div style={styleContainer}>
         <h3>{`${this.state.op1} + ${this.state.op2}`}</h3>
-        <input value={this.state.min} onChange={this.onChangeMin} />
-        <input value={this.state.max} onChange={this.onChangeMax} />
-        <input value={this.state.value} onChange={this.onChange} />
+        <div>
+          <input
+            placeholder="min"
+            style={styleInput}
+            value={this.state.min}
+            onChange={this.onChangeMin}
+          />
+          <input
+            placeholder="max"
+            style={styleInput}
+            value={this.state.max}
+            onChange={this.onChangeMax}
+          />
+        </div>
+        <div>
+          <input
+            style={styleInput}
+            value={this.state.value}
+            onChange={this.onChange}
+          />
+        </div>
         {button}
-        <div>{answer}</div>
+        {answer}
       </div>
     );
   }
