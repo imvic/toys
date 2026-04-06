5565ed4c7499afb493583f5b1235eafbfa39ba2f
imvicx <hegna.he@outlook.com>
2026-04-06 08:44:25 +0800

    chore: add index

--- packages/stktoys/webpack.config.common.local.js
+++ packages/stktoys/webpack.config.common.local.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const config = require("./webpack.config.common");
 
-const outputPath = `${__dirname}/release`;
+const outputPath = `${__dirname}/../..`;
 config.output.path = outputPath;
 
 module.exports = config;
